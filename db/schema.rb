e556aa91c251dd0707fc1c7550b3f4a52c55a608
Bill Garr <bgarr.thinkabt@gmail.com>
2019-07-26 16:18:16 -0400

    fix shl_case_number fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_144739) do
-
+ActiveRecord::Schema.define(version: 2019_07_26_192306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,10 +36,23 @@ ActiveRecord::Schema.define(version: 2019_07_26_144739) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "population_estimates", force: :cascade do |t|
+    t.boolean "raw", default: true, null: false
+    t.date "sample_date"
+    t.string "shl_case_number"
+    t.date "spawning_date"
+    t.string "lifestage"
+    t.string "abundance"
+    t.string "facility"
+    t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "spawning_successes", force: :cascade do |t|
     t.boolean "raw", default: true, null: false
     t.string "tag"
-    t.decimal "shl_number"
+    t.string "shl_case_number"
     t.date "spawning_date"
     t.date "date_attempted"
     t.string "spawning_success"
@@ -48,5 +60,4 @@ ActiveRecord::Schema.define(version: 2019_07_26_144739) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
