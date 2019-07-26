907fa3ad3f3252d6616ae84c7495764a785a9240
Bill Garr <bgarr.thinkabt@gmail.com>
2019-07-26 15:02:25 -0400

    change type of spawing_successes shl_number to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_07_26_192306) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
