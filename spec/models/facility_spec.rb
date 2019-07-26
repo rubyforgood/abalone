dc01e37e056c176557d97bfdd97086e74b8ad1bc
Dr. Thunder <thunderheavyindustries@gmail.com>
2019-07-26 08:53:58 -0600

    set up factorybot/faker add spec for facility

--- spec/models/facility_spec.rb
+++ spec/models/facility_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Facility, type: :model do
+ let(:facility) { FactoryBot.create :facility }
 
-  
-
-
+ describe 'structure' do
+   it { is_expected.to have_db_column :name }
+   it { is_expected.to have_db_column :code }
+ end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
+require 'factory_bot'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
