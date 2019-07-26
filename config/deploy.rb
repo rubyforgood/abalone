f34e6944a28e710440c449c12168a862bd7d4e53
Brandon R <brice84@gmail.com>
2019-07-26 14:57:40 -0400

    Assume we're deploying master branch.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "abalone"
 set :repo_url, "git@github.com:rubyforgood/abalone.git"
 
 # Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/srv/www/abalone"
