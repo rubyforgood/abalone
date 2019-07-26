2696464e96a8ea738fb99b279e925c112feeb9f3
glassjoseph <glassjoseph@gmail.com>
2019-07-26 14:53:12 -0400

    Add wildCollection migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_140157) do
+ActiveRecord::Schema.define(version: 2019_07_26_182100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,35 @@ ActiveRecord::Schema.define(version: 2019_07_26_140157) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
 
-    create_table "facilities", force: :cascade do |t|
+  create_table "facilities", force: :cascade do |t|
     t.string "name"
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "wild_collections", force: :cascade do |t|
+    t.boolean "raw", default: true, null: false
+    t.string "tag"
+    t.date "collection_date"
+    t.string "general_location"
+    t.string "precise_location"
+    t.point "collection_coodinates"
+    t.string "proximity_to_nearest_neighbor"
+    t.string "collection_method_notes"
+    t.string "foot_condition_notes"
+    t.string "collection_depth"
+    t.string "length"
+    t.string "weight"
+    t.string "gonad_score"
+    t.string "predicted_sex"
+    t.string "initial_holding_facility"
+    t.string "final_holding_facility_date_of_arrival"
+    t.string "otc_treatment_completion_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
