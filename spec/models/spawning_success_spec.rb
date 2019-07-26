1397b26a3aecea7019cc23e89dd52fdda7bac346
Bill Garr <bgarr.thinkabt@gmail.com>
2019-07-26 11:51:10 -0400

    rename spec

--- spec/models/spawning_success_spec.rb
+++ spec/models/spawning_success_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SpawningSuccess, type: :model do
+RSpec.describe SpawningSuccessRawData, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
