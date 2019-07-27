f2ed8ba961dc1386564efd06dbe10f5a514b5f06
Dr. Thunder <thunderheavyindustries@gmail.com>
2019-07-27 12:43:46 -0600

    more clean up for query structure

--- app/lib/aggregates.rb
+++ app/lib/aggregates.rb
@@ -4,12 +4,18 @@ module Aggregates
     # total number of animals in the program, by spawning date and holding facility
     # spawndate acts as the cohort designator here
     def self.total_animals_by_spawndate_and_facility(spawning_date, facility)
-      PopulationEstimate.not_raw.where(facility: facility, spawning_date: spawning_date).pluck(:abundance).map(&:to_i).reduce(:+)
+      PopulationEstimate.not_raw
+        .where(facility: facility, spawning_date: spawning_date)
+        .pluck(:abundance)
+        .map(&:to_i).reduce(:+)
     end
 
     # spawning history of the broodstock (i.e., when we attempted to spawn them, were they successful in releasing gametes)
     def self.spawning_history(spawning_date, successful= %w[y Y])
-      query = SpawningSuccess.not_raw.where(spawning_date: spawning_date, spawning_success: successful).group_by(&:date_attempted)
+      query = SpawningSuccess.not_raw
+                .where(spawning_date: spawning_date, spawning_success: successful)
+                .group_by(&:date_attempted)
+
       query.each{ |k,v|  query[k] = v.map(&:spawning_success).count  }
     end
 
@@ -33,7 +39,10 @@ module Aggregates
 
     # mortality within a cohort/population over time
     def self.deaths_by_cohort_for_date_range(cohort, date_range)
-      query_results = MortalityTracking.not_raw.where(cohort: cohort, mortality_date: date_range ).group_by(&:mortality_date)
+      query_results = MortalityTracking.not_raw
+                        .where(cohort: cohort, mortality_date: date_range )
+                        .group_by(&:mortality_date)
+
       query_results.each{|k,v| query_results[k] = v.map(&:number_morts).sum }
     end
   end
