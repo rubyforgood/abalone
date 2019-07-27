7667647d173d0c365aa1168721ca4da4a984193a
Brandon R <brice84@gmail.com>
2019-07-27 15:09:04 -0400

    Don't use es6 features for now due to uglifier config.

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
   if ($navbarBurgers.length > 0) {
     $navbarBurgers.forEach( el => {
       el.addEventListener('click', () => {
 
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+        var target = el.dataset.target;
+        var $target = document.getElementById(target);
 
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
