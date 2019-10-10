80af85657078b0c027aee0476b7563ef251eb3f6
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-10-10 20:11:31 +0700

    Update test

--- spec/features/tagged_animal_assesment_job_spec.rb
+++ spec/features/tagged_animal_assesment_job_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 describe "upload process for TaggedAnimalAssessmentJob", type: :feature do
 
+  let(:upload_file) { "db/sample_data_files/tagged_animal_assessment/Tagged_assessment_12172018 (original).csv" }
+
   it "is succsess" do
     visit 'file_uploads/new'
-    expect(page).to have_content 'File Uploads'
+
+    select "Tagged Animal Assessment", from: 'category'
+    attach_file('input_file', Rails.root + upload_file)
+    click_on 'Submit'
+
+    expect(page).to have_content 'Successfully queued spreadsheet for import'
   end
 end
