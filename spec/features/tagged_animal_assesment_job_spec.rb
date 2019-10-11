b3b19b5a6d256ab13fbd18bdc987a9b9d7b668f0
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-10-11 11:21:54 +0700

    Simplify invalid file,  refractor long paths

--- spec/features/tagged_animal_assesment_job_spec.rb
+++ spec/features/tagged_animal_assesment_job_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe "upload CSV files for TaggedAnimalAssessmentJob", type: :feature do
 
-  let(:valid_file) { "db/sample_data_files/tagged_animal_assessment/Tagged_assessment_12172018 (original).csv" }
-  let(:invalid_headers_file) { "db/sample_data_files/tagged_animal_assessment/Tagged_assessment_03172018-invalid-header.csv" }
+  let(:dir_path) { "db/sample_data_files/tagged_animal_assessment"}
+  let(:valid_file) { "#{dir_path}/Tagged_assessment_12172018 (original).csv" }
+  let(:invalid_file) { "#{dir_path}/invalid-headers.csv" }
 
   context 'when user successfully uploads a CSV with no errors' do
     it "creates new ProcessedFile record with processed status " do
@@ -24,11 +25,11 @@ describe "upload CSV files for TaggedAnimalAssessmentJob", type: :feature do
       visit 'file_uploads/new'
 
       select "Tagged Animal Assessment", from: 'category'
-      attach_file('input_file', Rails.root + invalid_headers_file)
+      attach_file('input_file', Rails.root + invalid_file)
       click_on 'Submit'
 
-      expect(ProcessedFile.count).to eq 1
       processed_file = ProcessedFile.last
+      expect(ProcessedFile.count).to eq 1
       expect(processed_file.status).to eq "Failed"
       expect(processed_file.job_errors).to eq "Does not have valid headers. Data not imported!"
       expect(page).to have_content 'Successfully queued spreadsheet for import'
@@ -50,8 +51,8 @@ describe "upload CSV files for TaggedAnimalAssessmentJob", type: :feature do
       attach_file('input_file', Rails.root + valid_file)
       click_on 'Submit'
 
-      expect(ProcessedFile.count).to eq 2
       processed_file = ProcessedFile.where(status: "Failed").first
+      expect(ProcessedFile.count).to eq 2
       expect(processed_file.job_errors).to eq "Already processed a file with the same name. Data not imported!"
       expect(page).to have_content 'Successfully queued spreadsheet for import'
     end
