035ee2fae7f8d5f8cc440ac16281654a009d4013
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-10-21 15:33:46 +0700

    Add model validations specs

--- spec/models/tagged_animal_assessment_spec.rb
+++ spec/models/tagged_animal_assessment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TaggedAnimalAssessment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it { should validate_presence_of(:measurement_date) }
+    it { should validate_presence_of(:shl_case_number) }
+    it { should validate_presence_of(:spawning_date) }
+    it { should validate_presence_of(:tag) }
+    it { should validate_presence_of(:length) }
+
+    it { should validate_numericality_of(:length) }
+  end
 end
