7b26bc1691eb78f844e74853e8a7ca792a827b1b
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-10-21 15:35:43 +0700

    Fix TypeError: no implicit conversion of nil into String and make model tests pass

--- app/models/tagged_animal_assessment.rb
+++ app/models/tagged_animal_assessment.rb
@@ -22,10 +22,12 @@ class TaggedAnimalAssessment < ApplicationRecord
   validates :length, numericality: true
 
   def measurement_date=(measurement_date_str)
+    return unless measurement_date_str
     write_attribute(:measurement_date, DateTime.strptime(measurement_date_str, '%m/%d/%y'))
   end
 
   def spawning_date=(spawning_date_str)
+    return unless spawning_date_str
     write_attribute(:spawning_date, DateTime.strptime(spawning_date_str, '%m/%d/%y'))
   end
 
