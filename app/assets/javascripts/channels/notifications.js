281b8fcaa99db6987db26dfe658e695c1673e758
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-12-27 19:29:13 +0100

    List invalid headers and update notification type

--- app/assets/javascripts/channels/notifications.js
+++ app/assets/javascripts/channels/notifications.js
@@ -9,8 +9,17 @@ App.notifications = App.cable.subscriptions.create("NotificationsChannel", {
     // console.log("Disconnected to notifications")
   },
 
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-    document.getElementById("notification-message").innerHTML = data.html
+  received: function({ content: { invalid_headers, valid_headers, notification_type }}) {
+    const $notificationErrorType = $('.notification-error-type');
+    const $notificationErrorContent = $('.notification-error-content');
+
+    const wrap = tagName => contents => `<${tagName}>${Array.from(contents).join('')}</${tagName}>`
+    const ul = wrap('ul');
+    const li = wrap('li');
+
+    const list = items => ul(items.map(li));
+
+    $notificationErrorContent.html(list(invalid_headers));
+    $notificationErrorType.addClass(`is-${notification_type}`);
   }
 });
