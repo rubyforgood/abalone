8fabd263f81e75fdcdaa6c2faf5c8a0d4a66cff3
wesley wong <wesley@freeletics.com>
2020-01-16 17:01:29 +0100

    load seeds regardless, because while you're uploading the rows, some might be cached, then the complete facility seeds won't load

--- spec/jobs/wild_collection_job_spec.rb
+++ spec/jobs/wild_collection_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe WildCollectionJob do
   let(:filename) { "example_of_wild_collection_data.csv" }
 
   before do
-    Rails.application.load_seed if Facility.valid_codes.empty?
+    Rails.application.load_seed
   end
 
   it_behaves_like "import job"
