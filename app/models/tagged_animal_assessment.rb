f8c05f9080e77c570fae5c918662cb5866fef0b6
corneliusellen <ecorneli@iwu.edu>
2020-02-02 09:26:15 -0700

    wip

--- app/models/tagged_animal_assessment.rb
+++ app/models/tagged_animal_assessment.rb
@@ -25,18 +25,32 @@ class TaggedAnimalAssessment < ApplicationRecord
     write_attribute(:spawning_date, DateTime.strptime(spawning_date_str, '%m/%d/%y'))
   end
 
-  def self.lengths_for_measurement(processed_file_id)
-    # group by bin (1cm). need constant of bins
-      # groups = select(:length).where(processed_file_id: processed_file_id).map { |record| record.length.to_f }
-    # count = count of all animals from that spreadsheet
+  def self.lengths_for_measurement(shl_case_number, measurement_date)
+      measurements = select(:length)
+                     .where(shl_case_number: shl_case_number)
+                     .where(measurement_date: measurement_date)
 
-    # total = total number of estimated animals from cohort (will need PopulationEstimate minus Mortality)
+      # group by bin (1cm). need constant of bins
+      grouped_measurements = measuremnts.group_by{|record| record.length.to_i}
 
-    # for each group, num / count * total. will come up with a whole number, like 20. keep 20 with the size bin {"2cm" => 20}
+      # count = count of all animals from that spreadsheet
+      sample = measurements.count
 
-    # for each group, shovel in 2.times {data << 20} to an array
+      # total = total number of estimated animals from cohort (will need PopulationEstimate minus Mortality)
+      total = 100
 
-    # data = [20,20]
+      # for each group, num / count * total. will come up with a whole number, like 20. keep 20 with the size bin {"2cm" => 20}
+      extrapolated_grouped_measurements = grouped_measurements.map do |group|
+        { group.first => (group.last.count / sample * total).round }
+      end
+
+      # for each group, shovel in 2.times {data << 20} to an array
+      # data = [20,20]
+      extrapolated_lengths = []
+      extrapolated_grouped_measurements.each do |group|
+        group.value.times{ extrapolated_lengths << group.key }
+      end
+      extrapolated_lengths
   end
 
   def cleanse_data!
