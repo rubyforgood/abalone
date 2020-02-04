e0fefd4fd67d6ff0155aafa71260bca758892b94
corneliusellen <ecorneli@iwu.edu>
2020-02-04 10:30:38 -0700

    Pass in values to override factory. Fix BOM character again

--- spec/features/upload_untagged_animal_assessments_spec.rb
+++ spec/features/upload_untagged_animal_assessments_spec.rb
@@ -50,6 +50,8 @@ describe "upload UntaggedAnimalAssessment category", type: :feature do
   context 'when user upload a CSV that has been already processed' do
     before do
       FactoryBot.create :processed_file,
+        filename: 'Untagged_assessment_03122018.csv',
+        cateogry: 'Untagged Animal Assessment',
         status: 'Processed',
         temporary_file_id: temporary_file.id
     end
