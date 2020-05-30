dca6c1f844957d72c02e2c37ac95b9336404fc16
Zee <50284+zspencer@users.noreply.github.com>
2020-05-30 10:00:31 -0700

    Resolve warnings in Test Suite (#172)
    There were a few warnings occuring when the tests were ran, which were a
    bit rough to think through when reviewing for test failures.
    
    I believe they may also have been indicating that the expectations we
    thought we had in place were not actually in place.

--- spec/models/consolidation_report_spec.rb
+++ spec/models/consolidation_report_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ConsolidationReport, type: :model do
   it "ConsolidationReport has associations" do
     is_expected.to belong_to(:family)
-    is_expected.to belong_to(:tank_from), class_name: 'Tank'
-    is_expected.to belong_to(:tank_to), class_name: 'Tank'
+    is_expected.to belong_to(:tank_from).class_name('Tank')
+    is_expected.to belong_to(:tank_to).class_name('Tank')
   end
 end
