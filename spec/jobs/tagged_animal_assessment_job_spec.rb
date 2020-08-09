015508b48249a5c8a992c3f00b899bb875126fd4
mdworken <31595784+mdworken@users.noreply.github.com>
2020-08-09 00:35:20 -0400

    Fixes spec failure introduce by gem upgrade changing support for spaces in csv file names. (#227)

--- spec/jobs/tagged_animal_assessment_job_spec.rb
+++ spec/jobs/tagged_animal_assessment_job_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe TaggedAnimalAssessmentJob do
-  let(:filename) { "Tagged_assessment_12172018 (original).csv" }
+  let(:filename) { "Tagged_assessment_12172018(original).csv" }
 
   it_behaves_like "import job"
 end
