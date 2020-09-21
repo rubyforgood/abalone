04389acacd950050b2620dd3d181a45502a0a964
Brian Todt <todtb@users.noreply.github.com>
2020-09-21 15:33:46 -0400

    Add CRUD pages for family resource
    Co-authored-by: Hayden Rouille <hayden@rouille.net>
    Co-authored-by: Vinicius Gama <vinicius.sgama@gmail.com>
    Co-authored-by: Ayako Kurushima <ayakokurushima@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :facilities
+  resources :families
   resources :animals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
