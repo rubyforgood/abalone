6b32c98e029e73face4aadd743eb176b1e5715c3
Michelle Hey <michelle.hey@powerhrg.com>
2020-09-21 14:55:38 -0500

    setup destroy method in controller for tanks

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,11 +1,21 @@
 class TanksController < ApplicationController
-  before_action :set_tank, only: [:show]
+  before_action :set_tank, only: [:show, :destroy]
 
   def index
     @tanks = Tank.all
   end
 
   def show; end
+
+  # DELETE /families/1
+  # DELETE /families/1.json
+  def destroy
+    @tank.destroy
+    respond_to do |format|
+      format.html { redirect_to tanks_url, notice: 'Tank was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
 
