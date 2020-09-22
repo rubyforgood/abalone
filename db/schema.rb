f02aa0164ba2f0675cfd48a353e83f6bf2060576
Michelle Hey <michelle.hey@powerhrg.com>
2020-09-22 09:45:21 -0500

    added role column to users table and set default role as 'user'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_162226) do
+ActiveRecord::Schema.define(version: 2020_09_22_142446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -269,6 +269,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_162226) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "organization_id"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let!(:user) { create(:user) }
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  it "should set a default role of 'user' when created" do
+    expect(user.role).to eq :user
+    expect(user.role).to_not eq :admin
+  end
 end
