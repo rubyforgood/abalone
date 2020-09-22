9914b3649efcb1dbe290509c9c65df1756845f6a
Hayden Rouille <hayden@rouille.dev>
2020-09-22 13:29:07 -0700

    add organizations to operations, measurements and measurement events

--- spec/models/measurement_event_spec.rb
+++ spec/models/measurement_event_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe MeasurementEvent, type: :model do
   it "has associations" do
     is_expected.to belong_to(:tank).optional
     is_expected.to have_many(:measurements)
+    is_expected.to belong_to(:organization)
   end
 end
