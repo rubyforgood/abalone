0200f128025fccbeebaa52ee49b2f5567ffca518
Brian Todt <todtb@users.noreply.github.com>
2020-09-23 16:29:47 -0400

    Add csv upload index
    Add an index page scoped to organization that shows file uploads, names,
    timestamps, status.
    
    Co-authored-by: darrendc <darren.dcosta@powerhrg.com>
    Co-authored-by: Michelle Hey <michelle.hey@powerhrg.com>
    Co-authored-by: Benjamin Krigel <benjamin.krigel@gmail.com>

--- app/helpers/file_uploads_helper.rb
+++ app/helpers/file_uploads_helper.rb
@@ -1,2 +1,11 @@
 module FileUploadsHelper
+  def file_upload_status_style(status)
+    if status.include? '0 records had errors'
+      'fa fa-check has-text-success'
+    elsif status.include? 'Pending'
+      ''
+    elsif status.match /[1-9]\d* records had errors/
+      'fas fa-exclamation-triangle has-text-danger'
+    end
+  end
 end
