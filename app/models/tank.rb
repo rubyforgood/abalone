16eb0caaee4471159317c93524120f8d9b147872
Hayden Rouille <hayden@rouille.dev>
2020-09-23 15:26:39 -0700

    remove unused associations

--- app/models/tank.rb
+++ app/models/tank.rb
@@ -3,7 +3,6 @@ class Tank < ApplicationRecord
 
   belongs_to :facility, optional: true
   has_many :operations, dependent: :destroy
-  has_many :post_settlement_inventories
   has_many :measurements, as: :subject
 
   has_one :family, required: false
