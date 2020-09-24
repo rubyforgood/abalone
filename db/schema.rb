904b08a63aa6af642e9873c772c1c45586ed500d
Hayden Rouille <hayden@rouille.dev>
2020-09-24 09:49:43 -0700

    remove bad schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_202015) do
+ActiveRecord::Schema.define(version: 2020_09_24_145950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_202015) do
     t.index ["animal_id"], name: "index_animals_shl_numbers_on_animal_id"
     t.index ["shl_number_id"], name: "index_animals_shl_numbers_on_shl_number_id"
   end
-  
+
   create_table "cohorts", force: :cascade do |t|
     t.bigint "female_id"
     t.bigint "male_id"
@@ -224,17 +224,6 @@ ActiveRecord::Schema.define(version: 2020_09_23_202015) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organization_id"], name: "index_users_on_organization_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "versions", force: :cascade do |t|
-    t.string "item_type", null: false
-    t.bigint "item_id", null: false
-    t.string "event", null: false
-    t.string "whodunnit"
-    t.text "object"
-    t.datetime "created_at"
-    t.text "object_changes"
-    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
