c18481fd2ecfb35b7f99504a6757744e5fd4e4e6
Hayden Rouille <hayden@rouille.dev>
2020-09-24 08:48:34 -0700

    Add specs for shl numbers
    Co-authored-by: Brian Todt <todtb@users.noreply.github.com>
    Co-authored-by: Michelle Hey <michelle.hey@powerhrg.com>

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Animal, type: :model do
   it "Animal has associations" do
     is_expected.to have_many(:measurements)
     is_expected.to belong_to(:organization)
+    is_expected.to have_many(:animals_shl_numbers).dependent(:destroy)
+    is_expected.to have_many(:shl_numbers).through(:animals_shl_numbers)
   end
 end
