347d07054a276868432650957c8a6719927982eb
Hayden Rouille <hayden@rouille.dev>
2020-09-25 07:13:52 -0400

    Styleguide

--- spec/factories/animals.rb
+++ spec/factories/animals.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     collection_year { 1 }
     date_time_collected { "2020-04-04 18:52:09" }
     collection_position { "MyString" }
-    sequence(:tag) { |n| "G#{sprintf('%03d', n)}" }
+    sequence(:tag) { |n| "G#{format('%<number>03d', number: n)}" }
     sex { Animal.sexes.keys.sample }
     organization
     cohort
@@ -12,7 +12,7 @@ FactoryBot.define do
     collection_year { 1 }
     date_time_collected { "2020-04-04 18:52:09" }
     collection_position { "MyString" }
-    sequence(:tag) { |n| "Y#{sprintf('%03d', n)}" }
+    sequence(:tag) { |n| "Y#{format('%<number>03d', number: n)}" }
     sex { 'male' }
     organization
   end
@@ -20,7 +20,7 @@ FactoryBot.define do
     collection_year { 1 }
     date_time_collected { "2020-04-04 18:52:09" }
     collection_position { "MyString" }
-    sequence(:tag) { |n| "R#{sprintf('%03d', n)}" }
+    sequence(:tag) { |n| "R#{format('%<number>03d', number: n)}" }
     sex { 'female' }
     organization
   end
