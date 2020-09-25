0a7c52ef0d288e1d8cb101fb5ab6e8389a0ea133
Hayden Rouille <hayden@rouille.dev>
2020-09-25 06:56:07 -0400

    Add real life examples to specs

--- spec/factories/animals.rb
+++ spec/factories/animals.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     collection_year { 1 }
     date_time_collected { "2020-04-04 18:52:09" }
     collection_position { "MyString" }
-    sequence(:tag) { |n| "G#{n}" }
+    sequence(:tag) { |n| "G#{sprintf('%03d', n)}" }
     sex { Animal.sexes.keys.sample }
     organization
     cohort
@@ -12,7 +12,7 @@ FactoryBot.define do
     collection_year { 1 }
     date_time_collected { "2020-04-04 18:52:09" }
     collection_position { "MyString" }
-    sequence(:tag) { |n| "Y#{n}" }
+    sequence(:tag) { |n| "Y#{sprintf('%03d', n)}" }
     sex { 'male' }
     organization
   end
@@ -20,7 +20,7 @@ FactoryBot.define do
     collection_year { 1 }
     date_time_collected { "2020-04-04 18:52:09" }
     collection_position { "MyString" }
-    sequence(:tag) { |n| "R#{n}" }
+    sequence(:tag) { |n| "R#{sprintf('%03d', n)}" }
     sex { 'female' }
     organization
   end
