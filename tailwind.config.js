27ee26e60709e48132ee343b853e7012e1295998
Michelle Hey <michelle.hey@powerhrg.com>
2020-09-25 12:00:57 -0500

    add tailwind to animals

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  theme: {
+    boxShadow: {
+      sm: "0 4px 10px rgba(60,106,139,0.15)",
+    },
+    container: {
+      center: true,
+    },
+    prefix: "tw-",
+    fontSize: {
+      title1: "2.5rem",
+      title2: "2.25rem",
+      title3: "1.875rem",
+      body: "1rem",
+      caption: "0.875rem",
+    },
+    extend: {
+      colors: {
+        "primary-dark": "#6767ca",
+        background: "#f3f7fa",
+        primary: "#9d9dd3",
+        secondary: "#eb5757",
+        dark: "#333333",
+        light: "#ffffff",
+        warning: "#f2994a",
+        success: "#27ae60",
+        "dark-light": "#eee",
+        "caption-light": "#9191A2",
+        "caption-dark": "#ACACC1",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
