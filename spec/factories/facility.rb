d27c0e24754b9da4dc599b88bbf7c84cb3f12552
todtb <todtb@users.noreply.github.com>
2020-10-01 11:38:07 -0400

    Autocorrect via rubocop

--- spec/factories/facility.rb
+++ spec/factories/facility.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :facility do
-    name { "#{Faker::Games::HalfLife.location}" }
+    name { Faker::Games::HalfLife.location.to_s }
     code { name.gsub(/[^A-Z]/, '') }
     organization
   end
