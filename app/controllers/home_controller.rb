a3dd3048a85a44d4fbab91fdcaf54eb29ea2e57d
PuZZleDucK <puzzleduck@gmail.com>
2020-10-02 21:46:37 +1000

    simplifiy conditional assignment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,9 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    if current_user
-      @facility_count = current_user.organization.facilities.count
-      @cohort_count = current_user.organization.cohorts.count
-      @animal_count = current_user.organization.animals.count
-    else
-      @facility_count = 3
-      @cohort_count = 7
-      @animal_count = 41
-    end
+    @facility_count = current_user&.organization&.facilities&.count || 3
+    @cohort_count = current_user&.organization&.cohorts&.count || 7
+    @animal_count = current_user&.organization&.animals&.count || 41
   end
 
   def show; end
