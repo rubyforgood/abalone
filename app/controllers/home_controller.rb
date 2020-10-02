cc92a43f64f688f90feff3b89109e2f695c98333
PuZZleDucK <puzzleduck@gmail.com>
2020-10-02 22:18:03 +1000

    convert conditional assignment into conditional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,15 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     # Retrive current user stats or assign defaults
-    @facility_count = current_user&.organization&.facilities&.count || 3
-    @cohort_count = current_user&.organization&.cohorts&.count || 7
-    @animal_count = current_user&.organization&.animals&.count || 41
+    if current_user
+      @facility_count = current_user.organization.facilities.count
+      @cohort_count = current_user.organization.cohorts.count
+      @animal_count = current_user.organization.animals.count
+    else
+      @facility_count = 3
+      @cohort_count = 7
+      @animal_count = 41
+    end
   end
 
   def show; end
