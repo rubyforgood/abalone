630b890a94e5bedbc6324ccd43285f06ccad8576
PuZZleDucK <puzzleduck@gmail.com>
2020-10-02 21:42:30 +1000

    refactor logic into controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
-  def index; end
+  def index
+    if current_user
+      @facility_count = current_user.organization.facilities.count
+      @cohort_count = current_user.organization.cohorts.count
+      @animal_count = current_user.organization.animals.count
+    else
+      @facility_count = 3
+      @cohort_count = 7
+      @animal_count = 41
+    end
+  end
 
   def show; end
 end
