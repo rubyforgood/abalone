50e84383efbbfab703bb0f82db006084da632a9a
PuZZleDucK <puzzleduck@gmail.com>
2020-10-02 21:35:03 +1000

    improve tests with example data

--- spec/features/home_page_statistics_spec.rb
+++ spec/features/home_page_statistics_spec.rb
@@ -2,21 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Home Page Statistics' do
   describe 'view home page statistics', type: :feature do
-    let(:user) { create(:user) }
+    let(:organization) { create(:organization) }
+    let(:user) { create(:user, organization: organization) }
+    let(:animal) { create(:animal, organization: organization) }
+    let(:facility) { create(:facility, organization: organization) }
+    let(:cohort) { create(:cohort, organization: organization) }
 
     it 'When I view the home page as a visitor I see example data' do
       visit root_path
-      expect(page.all('.card-content')[0].find('.title').text).to eq('41')
-      expect(page.all('.card-content')[1].find('.title').text).to eq('3')
-      expect(page.all('.card-content')[2].find('.title').text).to eq('7')
+      expect(page.all('.card-content')[0].find('.title').text).to eq('41') # Total No. of Animals
+      expect(page.all('.card-content')[1].find('.title').text).to eq('3') # No. of Facilities
+      expect(page.all('.card-content')[2].find('.title').text).to eq('7') # No. of Spawn Dates
     end
 
     it 'When I login and view the home page I see my company data' do
       sign_in user
+      animal.reload
+      facility.reload
+      cohort.reload
       visit root_path
-      expect(page.all('.card-content')[0].find('.title').text).to eq('0')
-      expect(page.all('.card-content')[1].find('.title').text).to eq('0')
-      expect(page.all('.card-content')[2].find('.title').text).to eq('0')
+      expect(page.all('.card-content')[0].find('.title').text).to eq('1')
+      expect(page.all('.card-content')[1].find('.title').text).to eq('1')
+      expect(page.all('.card-content')[2].find('.title').text).to eq('1')
     end
   end
 end
