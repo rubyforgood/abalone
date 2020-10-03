23cae9bbf358b971aaca7be9a21242a712876863
PuZZleDucK <puzzleduck@gmail.com>
2020-10-03 22:26:32 +1000

    prefer guard clause over conditional

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    # Retrive current user stats or assign defaults
-    if current_user
-      @facility_count = current_user.organization.facilities.count
-      @cohort_count = current_user.organization.cohorts.count
-      @animal_count = current_user.organization.animals.count
-    end
+    return unless current_user
+
+    # Retrive current organization stats
+    @facility_count = current_user.organization.facilities.count
+    @cohort_count = current_user.organization.cohorts.count
+    @animal_count = current_user.organization.animals.count
   end
 
   def show; end
