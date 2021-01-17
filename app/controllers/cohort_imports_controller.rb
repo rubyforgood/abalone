05c82d3a7f388464d1d6f18cf28bd03a62e50eba
Nick Schimek <cfme00@gmail.com>
2021-01-17 18:22:04 +0900

    Implement csv import functionality

--- app/controllers/cohort_imports_controller.rb
+++ app/controllers/cohort_imports_controller.rb
@@ -2,7 +2,15 @@ class CohortImportsController < ApplicationController
   def new; end
 
   def create
-    Cohort.create! organization: current_organization
-    redirect_to cohorts_path
+    if params[:cohort_csv].content_type == 'text/csv'
+      upload = FileUpload.create(user: current_user, organization: current_organization, status: 'Pending',
+                                 file: params[:cohort_csv])
+
+      ImportCohortsJob.perform_later(upload)
+
+      redirect_to cohorts_path, notice: 'Processing file...'
+    else
+      redirect_to csv_upload_enclosures_path, error: 'Invalid file type. Please upload a CSV.'
+    end
   end
 end
