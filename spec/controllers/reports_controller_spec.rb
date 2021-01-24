6ac814182f35ef50e4ee2cb9df0ce3a8e053515b
Craig Zaccaro <czaccaro@live.com>
2021-01-24 09:52:15 -0500

    Update specs.

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,18 +1,20 @@
-require 'rails_helper'
+# Replaced by blazer reporting - 1/24/21
 
-describe ReportsController do
-  include Devise::Test::ControllerHelpers
-
-  let(:user) { FactoryBot.create(:user) }
-
-  before do
-    sign_in user
-  end
-
-  describe '#index' do
-    it 'should have response code 200' do
-      get :index
-      expect(response.code).to eq '200'
-    end
-  end
-end
+# require 'rails_helper'
+#
+# describe ReportsController do
+#   include Devise::Test::ControllerHelpers
+#
+#   let(:user) { FactoryBot.create(:user) }
+#
+#   before do
+#     sign_in user
+#   end
+#
+#   describe '#index' do
+#     it 'should have response code 200' do
+#       get :index
+#       expect(response.code).to eq '200'
+#     end
+#   end
+# end
