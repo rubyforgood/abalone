746a117c961c260c4b0eabec7c96393f11da14ca
Monica <monica.mateiu@gmail.com>
2021-02-23 21:43:44 +0000

    fix table accessibility issues

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -27,6 +27,7 @@ describe "When I visit the user index page" do
       expect(page).to have_content user.email
       expect(page).to have_content user.role
       expect(page).to have_content user.organization.name
+      expect(page).to have_content 'Actions'
     end
   end
 
