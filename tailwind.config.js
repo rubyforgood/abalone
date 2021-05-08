20c5cd95f3eace151e862d1da0a5e0d8164e0e4b
Libby Rodriguez <librod89@gmail.com>
2021-05-07 20:48:21 -0600

    Replace bulma with tailwind on home/index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   theme: {
     boxShadow: {
+      default: "0 2px 3px rgb(10 10 10 / 10%), 0 0 0 1px rgb(10 10 10 / 10%)",
       sm: "0 4px 10px rgba(60,106,139,0.15)",
     },
     container: {
@@ -11,6 +12,7 @@ module.exports = {
       title1: "2.5rem",
       title2: "2.25rem",
       title3: "1.875rem",
+      title4: "1.25rem",
       body: "1rem",
       caption: "0.875rem",
     },
@@ -27,6 +29,9 @@ module.exports = {
         "dark-light": "#eee",
         "caption-light": "#757586",
         "caption-dark": "#ACACC1",
+      },
+      spacing: {
+        "72": "18rem",
       },
     },
   },
