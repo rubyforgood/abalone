7a4d4413cb1f592c76968965550dbbdb768254ab
CraigJZ <CraigJZ@users.noreply.github.com>
2021-06-24 20:23:49 -0400

    Remove extra space in spec_helper to make rubocop happy.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,6 @@
 require 'simplecov'
 SimpleCov.start
 
-
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
