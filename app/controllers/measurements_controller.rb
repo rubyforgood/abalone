3289b62e8fb8c35fa5137a4676228f261da087d1
Jo Fischer <josephinefischer9@gmail.com>
2021-10-13 10:40:18 -0400

    Submit form and check admin authorization

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,17 +1,38 @@
 class MeasurementsController < ApplicationController
+  before_action :authorize_admin!, only: %i[edit]
+  before_action :set_measurement, only: %i[show edit update]
+
   def index
     @measurements = Measurement.all
   end
 
-  def show
-    @measurement = Measurement.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @measurement = Measurement.find(params[:id])
-  end
+  def edit; end
 
-  def update; end
+  def update
+    if @measurement.update(measurement_params)
+      redirect_to @measurement, notice: 'Measurement was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
   def new; end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_measurement
+    @measurement = Measurement.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def measurement_params
+    params.require(:measurement).permit(
+      :value,
+      :subject_type,
+      :measurement_type_id
+    ).merge(organization_id: current_organization.id)
+  end
 end
