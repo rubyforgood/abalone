a8c7e0bd0e768cad9845e4c9430dda71903426c0
Jo Fischer <josephinefischer9@gmail.com>
2021-10-14 09:54:26 -0400

    Fix from running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -323,8 +323,7 @@ ActiveRecord::Schema.define(version: 2021_10_11_181422) do
   end
 
   create_table "versions", force: :cascade do |t|
-    t.string "item_type"
-    t.string "{:null=>false}"
+    t.string "item_type", null: false
     t.bigint "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
