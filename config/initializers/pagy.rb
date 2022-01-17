eb73b63978463e5f57ee86909719b2afd3435fa7
Craig Zaccaro <czaccaro@live.com>
2022-01-16 19:11:53 -0500

    Fix rubocop.

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -3,21 +3,15 @@
 # Pagy initializer file (5.8.0)
 # Customize only what you really need and notice that the core Pagy works also without any of the following lines.
 # Should you just cherry pick part of this file, please maintain the require-order of the extras
-
-
 # Pagy DEFAULT Variables
 # See https://ddnexus.github.io/pagy/api/pagy#variables
 # All the Pagy::DEFAULT are set for all the Pagy instances but can be overridden per instance by just passing them to
 # Pagy.new|Pagy::Countless.new|Pagy::Calendar::*.new or any of the #pagy* controller methods
-
-
 # Instance variables
 # See https://ddnexus.github.io/pagy/api/pagy#instance-variables
 # Pagy::DEFAULT[:page]   = 1                                  # default
 # Pagy::DEFAULT[:items]  = 20                                 # default
 # Pagy::DEFAULT[:outset] = 0                                  # default
-
-
 # Other Variables
 # See https://ddnexus.github.io/pagy/api/pagy#other-variables
 # Pagy::DEFAULT[:size]       = [1,4,4,1]                       # default
@@ -28,12 +22,8 @@
 # Pagy::DEFAULT[:link_extra] = 'data-remote="true"'            # example
 # Pagy::DEFAULT[:i18n_key]   = 'pagy.item_name'                # default
 # Pagy::DEFAULT[:cycle]      = true                            # example
-
-
 # Extras
 # See https://ddnexus.github.io/pagy/extras
-
-
 # Backend Extras
 
 # Array extra: Paginate arrays efficiently, avoiding expensive array-wrapping and without overriding
@@ -111,8 +101,6 @@
 # require 'pagy/extras/searchkick'
 # uncomment if you are going to use Searchkick.pagy_search
 # Searchkick.extend Pagy::Searchkick
-
-
 # Frontend Extras
 
 # Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
@@ -148,8 +136,6 @@
 # Multi size var used by the *_nav_js helpers
 # See https://ddnexus.github.io/pagy/extras/navs#steps
 # Pagy::DEFAULT[:steps] = { 0 => [2,3,3,2], 540 => [3,5,5,3], 720 => [5,7,7,5] }   # example
-
-
 # Feature Extras
 
 # Gearbox extra: Automatically change the number of items per page depending on the page number
@@ -186,16 +172,11 @@
 # See https://ddnexus.github.io/pagy/extras/standalone
 # require 'pagy/extras/standalone'
 # Pagy::DEFAULT[:url] = 'http://www.example.com/subdir'  # optional default
-
-
 # Rails
-
 # Rails: extras assets path required by the helpers that use javascript
 # (pagy*_nav_js, pagy*_combo_nav_js, and pagy_items_selector_js)
 # See https://ddnexus.github.io/pagy/extras#javascript
 # Rails.application.config.assets.paths << Pagy.root.join('javascripts')
-
-
 # I18n
 
 # Pagy internal I18n: ~18x faster using ~10x less memory than the i18n gem
@@ -224,8 +205,6 @@
 #                 { locale: 'xyz',  # not built-in
 #                   filepath: 'path/to/pagy-xyz.yml',
 #                   pluralize: lambda{ |count| ... } )
-
-
 # I18n extra: uses the standard i18n gem which is ~18x slower using ~10x more memory
 # than the default pagy internal i18n (see above)
 # See https://ddnexus.github.io/pagy/extras/i18n
@@ -233,7 +212,5 @@
 
 # Default i18n key
 # Pagy::DEFAULT[:i18n_key] = 'pagy.item_name'   # default
-
-
 # When you are done setting your own default freeze it, so it will not get changed accidentally
 # Pagy::DEFAULT.freeze
